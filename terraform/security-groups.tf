f21a0bb4344c988ad42f7f48bb338d487e19937f
ghanatava <ghanatva@gmail.com>
2024-01-02 12:27:11 +0530

    added terraform/security-groups.tf

---
+++ terraform/security-groups.tf
@@ -0,0 +1,98 @@
+resource "aws_security_group" "web-sg" {
+  name        = "web-sg"
+  description = "web-sg"
+  vpc_id      = aws_vpc.todo-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "nlb-sg" {
+  name        = "nlb-sg"
+  description = "nlb-sg"
+  vpc_id      = aws_vpc.todo-vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web-sg.id]
+  }
+}
+
+resource "aws_security_group" "django-sg" {
+  name        = "django-sg"
+  description = "django-sg"
+  vpc_id      = aws_vpc.todo-vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web-sg.id]
+  }
+  ingress {
+    from_port       = 0
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.nlb-sg.id]
+  }
+
+  egress {
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.nlb-sg.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "nlb" {
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 8000
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.django-sg.id
+  security_group_id        = aws_security_group.nlb-sg.id
+}
+
+resource "aws_security_group" "db-sg" {
+  name        = "db-sg"
+  description = "db-sg"
+  vpc_id      = aws_vpc.todo-vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.django-sg.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
