0362195f091a14ee54788613926c60c0a707383a
ghanatava <ghanatva@gmail.com>
2024-01-02 14:10:08 +0530

    yo!

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -4,13 +4,13 @@ resource "aws_security_group" "web-sg" {
   vpc_id      = aws_vpc.todo-vpc.id
 
   ingress {
-    from_port   = 0
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 0
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
@@ -30,7 +30,7 @@ resource "aws_security_group" "nlb-sg" {
   vpc_id      = aws_vpc.todo-vpc.id
 
   ingress {
-    from_port       = 0
+    from_port       = 8000
     to_port         = 8000
     protocol        = "tcp"
     security_groups = [aws_security_group.web-sg.id]
@@ -43,13 +43,13 @@ resource "aws_security_group" "django-sg" {
   vpc_id      = aws_vpc.todo-vpc.id
 
   ingress {
-    from_port       = 0
+    from_port       = 22
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.web-sg.id]
   }
   ingress {
-    from_port       = 0
+    from_port       = 8000
     to_port         = 8000
     protocol        = "tcp"
     security_groups = [aws_security_group.nlb-sg.id]
@@ -71,7 +71,7 @@ resource "aws_security_group" "django-sg" {
 
 resource "aws_security_group_rule" "nlb" {
   type                     = "egress"
-  from_port                = 0
+  from_port                = 8000
   to_port                  = 8000
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.django-sg.id
@@ -84,7 +84,7 @@ resource "aws_security_group" "db-sg" {
   vpc_id      = aws_vpc.todo-vpc.id
 
   ingress {
-    from_port       = 0
+    from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
     security_groups = [aws_security_group.django-sg.id]
